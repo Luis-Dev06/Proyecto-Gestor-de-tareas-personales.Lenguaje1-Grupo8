df9bbd881514962f6e9cba3371cd678e871519b3
MiguelCO2 <miguelcharmelo13@gmail.com>
2025-12-10 17:38:29 -0400

    [Añadidos y mejoras en comentario identificativo] Miguel Charmelo 31951249

--- Gestor-De-Tareas-Personales.c
+++ Gestor-De-Tareas-Personales.c
@@ -1,13 +1,23 @@
 /*
-Los integrantes son:
+
+Carrera: Ing de Sistemas
+Semestre 4
+Materia: Lenguaje de programacion I
+Seccion: D3
+Docente: Juan Jose Rial Somma
+Grupo 8
+
+Integrantes:
+-Miguel Antonio 31951249
 -Luis Díaz 31755591
 
-
 */
+
 #include <stdio.h>
 int main(){
 
 
+    
     printf("Bienvenido al Gestor de Tareas Personales\n");
 
 return 0;

a22503c87e53feb72b793b9456ab7503e5067779
GabrielC-Cyber <gabrielaponte274@gmail.com>
2025-12-10 17:58:21 -0400

    [Añadido de comentarios y listado de funcionalidades] Gabriel Aponte 31680138

--- Gestor-De-Tareas-Personales.c
+++ Gestor-De-Tareas-Personales.c
@@ -10,15 +10,25 @@ Grupo 8
 Integrantes:
 -Miguel Antonio 31951249
 -Luis Díaz 31755591
+-Gabriel Aponte 31680138
 
 */
 
 #include <stdio.h>
 int main(){
 
-
+printf("=====================================\n");
     
     printf("Bienvenido al Gestor de Tareas Personales\n");
+
+    printf("Aqui podra:\n");
+    printf("1. Registrar tareas\n");
+    printf("2. Marcar tareas completadas\n");
+    printf("3. Mostrar tareas activas y complementarias\n");
+    printf("4. Guardar en archivo\n");
+    printf("5. Contar tareas activas\n");
+    printf("=====================================\n");
+
 
 return 0;
 }

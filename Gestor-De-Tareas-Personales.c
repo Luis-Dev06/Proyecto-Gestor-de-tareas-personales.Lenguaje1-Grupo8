a6c919f01f5fe1d847ae6fa4096685dc7b442590
GabrielC-Cyber <gabrielaponte274@gmail.com>
2026-01-24 18:57:47 -0400

    Estructura del programa y prototipos de funciones Gabriel Aponte 31.680.138

--- Gestor-De-Tareas-Personales.c
+++ Gestor-De-Tareas-Personales.c
@@ -15,6 +15,23 @@ Integrantes:
 */
 
 #include <stdio.h>
+#include <string.h>
+#define MAXIMO_TAREAS 100
+
+typedef struct 
+{
+    char titulo_tarea[100];
+    int prioridad; // 1-prioridad alta, 2-prioridad media, 3 prioridad baja
+    int estado_tarea; // 0-incompleta, 1-competa 
+}Tarea;
+
+Tarea Tareas[MAXIMO_TAREAS];
+
+int contador_tareas = 0;
+
+void agregartarea();
+
+
 int main(){
 
 printf("=====================================\n");
@@ -23,12 +40,13 @@ printf("=====================================\n");
 
     printf("Aqui podra:\n");
     printf("1. Registrar tareas\n");
-    printf("2. Marcar tareas completadas\n");
-    printf("3. Mostrar tareas activas y complementarias\n");
-    printf("4. Guardar en archivo\n");
-    printf("5. Contar tareas activas\n");
+    printf("2. Marcar tareas completadas(no activo)\n");
+    printf("3. Mostrar tareas activas y complementarias(no activo)\n");
+    printf("4. Guardar en archivo (no activo)\n");
+    printf("5. Contar tareas activas (no activo)\n");
     printf("=====================================\n");
-
+    
+agregartarea();
 
 return 0;
 }
